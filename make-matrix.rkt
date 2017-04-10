ec22a58e7df247a7a86277d5c14db2ee2b267a58
Dustin Gulley <dustingulley@Dustins-MacBook-Pro.local>
2017-04-09 22:39:57 -0400

    working on matrix

--- make-matrix.rkt
+++ make-matrix.rkt
@@ -8,14 +8,38 @@
 (define make-matrix
   (lambda (row-count col-count)
     (let ((a-matrix (quote ())))
-      (letrec ((make-matrix-h (lambda (rows cols)
-                                (cond ((and (> rows 0) (> cols 0)) (cons (quote ()) (make-matrix-h rows (- cols 1))))
-                                      ((and (> rows 0) (= cols 0)) (make-matrix-h (- rows 1) col-count))
+      (letrec ((make-matrix-h (lambda (matrices)
+                                (cond ((> matrices 0) (cons (quote ()) (make-matrix-h (- matrices 1))))
                                       (else (quote ()))))))
-        (let () (set! a-matrix (make-matrix-h row-count col-count)))
+        (let () (set! a-matrix (make-matrix-h (* row-count col-count))))
+        (letrec ((count (lambda ()
+                          (letrec ((count-h (lambda (count a-matrix)
+                                              (cond ((null? a-matrix) count)
+                                                    (else (count-h (+ count 1) (cdr a-matrix)))))))
+                            (count-h 0 a-matrix))))
+                 (get-row-col (lambda (a-row a-col)
+                                (letrec ((get-row-col-h (lambda (matrices a-matrix)
+                                                          (cond ((> matrices 0) (get-row-col-h (- matrices 1) (cdr a-matrix)))
+                                                                (else (car a-matrix))))))
+                                  (get-row-col-h (* a-row a-col)))))
+                 (set-row-col! (lambda a-row a-col a-node)
+                               (letrec ((set-row-col-h! (lambda (matrices matrix)
+                                                          (cond ((= matrices 0) (cons a-node (set-row-col-h! matrices)))
+                                                                ((not (null? matrix)) (cons (car matrix) (set-row-col-h! (- matrices 1))))
+                                                                (else matrix)))))
+                                 (set! a-matrix (set-row-col-h! (* a-row a-col) a-matrix))))
+                 
+                 )
         (lambda (message)
           (cond ((eq? message 'get-matrix) a-matrix)
-                (else (quote ()))))))))
+                ((eq? message 'get-row-col) (lambda (a-row a-col) (get-row-col a-row a-col)))
+                ((eq? message 'set-row-col!) (lambda (a-row a-col a-node) (set-row-col! a-row a-col a-node)))
+                ((eq? message 'count) (count))
+                ((eq? message '*) (lambda (a-matrix) (multiply a-matrix))) ;todo
+                ((eq? message 'set!) (lambda (a-matrix) (set-matrix! a-matrix))) ;todo
+                ((eq? message 'get-col) (lambda (a-col) (get-col a-col))) ;todo
+                ((eq? message 'get-row) (lambda (a-row) (get-row a-row))) ;todo
+                (else (quote ())))))))))
 
 ; Provide access to this module within other files
 (provide make-matrix)
