61b2eb23d32a56ae57c646f7e2adab5c0aca3f42
Dustin Gulley <dustingulley@Dustins-MacBook-Pro.local>
2017-04-10 23:32:05 -0400

    Worked on multiplying and fixedbugs in getting a row,there are probably bugs in getting the col

--- scratchpad.rkt
+++ scratchpad.rkt
@@ -7,4 +7,20 @@
 
 (define neural-net (make-neural-network 3 3 3 0.3))
 
-(define matrix (make-matrix 3 3))
+(define matrix-a (make-matrix 3 3))
+(define matrix-b (make-matrix 3 3))
+
+((matrix-a 'set!) '(1 1 1
+                    2 2 2
+                    3 3 3))
+
+((matrix-b 'set!) '(1 2 3
+                    1 2 3
+                    1 2 3))
+
+; expect
+; 18 18 18
+; 18 18 18
+; 18 18 18
+; ((matrix-a '*) matrix-b)
+; ((matrix-a 'get-row) 2)
