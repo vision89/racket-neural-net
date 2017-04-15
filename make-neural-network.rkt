48af1dd3687477828e444835e635bd26d342f91c
Dustin Gulley <dustingulley@Dustins-MacBook-Pro.local>
2017-04-15 00:36:18 -0400

    Turned everything into a little language

--- make-neural-network.rkt
+++ make-neural-network.rkt
@@ -1,34 +1,91 @@
 #lang racket
- (require math/base)
+(require math/base)
+(require "make-matrix.rkt")
 
 ; Author: Dustin Gulley
 ; Class: CSC 525
 ; Description: Project 4.  Initialize and return a neural network.
 ; Due date: April 26 2017
 
-; positive-int positive-int positive-int positive-float -> function
-; Initializes the neural network and returns a closure for manipulating the neural netowrk
-(define make-neural-network
-  (lambda (input-node-count hidden-node-count output-node-count learning-rate)
-    
+(define neural-net-lang
+  (letrec (
+
+           (matrix? (lambda (x)
+                      (cond ((eq? ':matrix (get-type x)) x)
+                            (else (quote ())))))
+
+           (neural-network? (lambda (x)
+                              (cond ((eq? ':neural-network (get-type x)) x)
+                                    (else (quote ())))))
+
+           (get-type
+            (lambda (x)
+              (car x)))
+
+           (get-input-node-count
+            (lambda (x)
+              (cadr x)))
+
+           (get-hidden-node-count
+            (lambda (x)
+              (caddr x)))
+
+           (get-output-node-count
+            (lambda (x)
+              (cadddr x)))
+
+           (get-learning-rate
+            (lambda (x)
+              (car (cddddr x))))
+
+           (get-weighted-hidden-input
+            (lambda (x)
+              (car (cdr (cddddr x)))))
+
+           (get-weighted-hidden-output
+            (lambda (x)
+              (car (cdr (cdr (cddddr x))))))
+           
+           (create-neural-network
+            (lambda (w x y z)
+              (list ':neural-network w x y z
+                    (matrix-lang 'new x x (list-of-random-vals (* x x) 100 100 1/2))
+                    (matrix-lang 'new y y (list-of-random-vals (* y y) 100 100 1/2)))))
+           
+           ; number -> number
+           ; Sigmoid function
+           (activation (lambda (x)
+                         (/ 1 (+ 1 (expt euler.0 (* x -1))))))
+
+           ; matrix matrix -> matrix
+           ; Train the neural network
+           (train (lambda (a-neural-network input-m target-m)
+                    (matrix-lang (matrix-lang (get-weighted-hidden-output a-neural-network) '*
+                                              (matrix-lang (matrix-lang (get-weighted-hidden-input a-neural-network) '*
+                                                                        input-m) 'apply activation)) 'apply activation)))
+
+           ; matrix -> matrix
+           ; Query the neural network
+           (query (lambda (a-neural-network input-m)
+                    (matrix-lang (matrix-lang (get-weighted-hidden-output a-neural-network) '*
+                                              (matrix-lang (matrix-lang (get-weighted-hidden-input a-neural-network) '*
+                                                                        input-m) 'apply activation)) 'apply activation)))
+           )
+
     ; <pattern> -> <varies>
-    ; Grammar for a little neural network language
-    ; given 'message, expect the identity function (todo: replace identity function)
-    ; given 'query, expect the identity function (todo: replace identity function)
+    ; Grammar for little neural network language
+    ; prototype (type input-node-count hidden-node-count output-node-count learning-rate weighted-hidden-output weighted-hidden-input)
+    ; 'new number number number number    -> neural-network
+    ; 'new number number                  -> neural-network
+    ; neural-network 'train matrix matrix -> matrix
+    ; neural-network 'quert matrix        -> matrix
     (match-lambda*
-
-        ; Train the neural network, I'm just returning identity while I develop this
-        ((list 'train a-var) a-var)
-
-        ; Query the neural network, I'm just returning identity while I develop this
-        ((list 'query a-var) a-var)
-
-        ; Default to null
-        (_ (quote ()))
-      )))
-
-(define sigmoid-function (lambda (x)
-                           (/ 1 (+ 1 (exp euler.0 (* x -1))))))
+      ((list 'new (? number? w) (? number? x) (? number? y) (? number? z)) (create-neural-network w x y z))
+      ((list 'new (? number? x) (? number? y)) (create-neural-network x x x y))
+      ((list (? neural-network? x) 'train (? matrix? y) (? matrix? z)) (train x y z))
+      ((list (? neural-network? x) 'query (? matrix? y)) (query x y))
+      (x x)
+      (_ #f))))
 
 ; Provide access to this module within other files
-(provide make-neural-network)
+(provide neural-net-lang)
--- scratchpad.rkt
+++ scratchpad.rkt
@@ -5,29 +5,10 @@
 (require "make-neural-network.rkt")
 (require "make-matrix.rkt")
 
-(define neural-net (make-neural-network 3 3 3 0.3))
+(define matrix-data-1 (matrix-lang 'new 3 3 '(1 3 1 2 2 3 3 1 2)))
+(define matrix-data-2 (matrix-lang 'new 3 3 '(1 2 3 3 2 1 1 3 2)))
 
-(define wih (make-matrix 3 3)) ; hidden input matrix
-(define wio (make-matrix 3 3)) ; hidden output matrix
+(println 'make-test-neural-net)
+(define test-neural-net (neural-net-lang 'new 3 3 3 0.3))
+(println 'querying-neural-net) (neural-net-lang test-neural-net 'query matrix-data-1)
 
-
-(wih '= (list-of-random-vals 9 100 100 1/2))
-(wio '= (list-of-random-vals 9 100 100 1/2))
-
-(println 'starting-tests)
-
-; Make test matrixes
-; given (1 3 1 2 2 3 3 1 2) * (1 2 3 3 2 1 1 3 2), expect (11 11 8 11 17 14 8 14 14)
-(define test-matrix-1 (make-matrix 3 3))
-(define test-matrix-2 (make-matrix 3 3))
-(define test-matrix-3 (make-matrix 3 3))
-
-(test-matrix-1 '= '(1 3 1 2 2 3 3 1 2))
-(test-matrix-2 '= 1 2 3 3 2 1 1 3 2)
-(test-matrix-3 '= 1 1 1 1 1 1 1 1 1)
-(print 'test-matrix-1:) (println (test-matrix-1))
-(print 'test-matrix-2:)(println (test-matrix-2))
-(print 'test-matrix-1-data:) (println (test-matrix-1 'as-list))
-(print 'test-matrix-2-data:)(println (test-matrix-2 'as-list))
-(print 'test-matrix-1*test-matrix-2:)(println ((test-matrix-1 '* test-matrix-2) 'as-list))
-(print 'test-matrix-1*test-matrix-2*test-matrix-3:)(println ((test-matrix-1 '* test-matrix-2 '* test-matrix-3) 'as-list))
